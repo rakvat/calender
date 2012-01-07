35747a4a6435bd6238c1f7b45b41cdd556659cc5
unknown <Christoph@rechner4.(none)>
2012-01-07 22:46:37 +0100

    Signed-off-by: unknown <Christoph@rechner4.(none)>

--- main.cs
+++ main.cs
@@ -4,7 +4,9 @@ public class Calender
 {
 	static public void Main ()
 	{
-		Console.WriteLine ("Hello Calender");
+	Console.WriteLine ("Hello Calender");
+        Console.WriteLine("Das Programm berechnet Kalender für die Jahre 1583 bis 4000");
+        Console.Write("Jahr eingeben ");
 	}
  
 }
