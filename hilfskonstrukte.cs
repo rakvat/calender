d97e9351460e4b6437c22050da95578e5bf3fe34
rumpelart <rumpelart@gmx.de>
2012-01-10 00:35:41 +0100

    wochentage

--- hilfskonstrukte.cs
+++ hilfskonstrukte.cs
@@ -9,6 +9,11 @@ namespace KalenderWelt
             {"Januar", "Februar", "Maerz", "April", 
              "Mai", "Juni", "Juli", "August", 
              "September", "Oktober", "November", "Dezember"};
+        public static string[] wochenTagNamen =
+            {"Montag", "Dienstag", "Mittwoch", 
+             "Donnerstag", "Freitag", "Samstag", "Sonntag"};
+        public static string[] wochenTagNamenKurz = 
+            {"MO", "DI", "MI", "DO", "FR", "SA", "SO"};
         //Schaltjahr wird spaeter korrigiert
         public static int[] tageImMonat = new int[12] 
             { 31, 28, 31, 30,   
