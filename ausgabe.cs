c6ed82d4e459937d44882f853633fa73486b0c9b
rumpelart <rumpelart@gmx.de>
2012-01-11 01:47:58 +0100

    wochentag berechnung refactoring, hoffentlich verständlicher und korrekter

--- ausgabe.cs
+++ ausgabe.cs
@@ -21,10 +21,8 @@ namespace KalenderWelt
 
         public void gibAus(int dasJahr)
         {
-            int monat = 0, tag = 0; //alle Veriablen fangen jetzt mit null an, das erspart das [monat-1] im Code
-            int wochentag = 0, jahre = 0, versatz = 0;
-            // Ja Fe Mä Ap Ma Ju Jl Ag Sp Ok Nv Dz, bei März versatz von 3 auf 4 ändern für Schaltjahren wie 2012
-            int[] monate = new int[12] { 0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5 };
+            int monat, tag; 
+            int wochentagDesErstenImMonat = 0; //Mo = 0, ...
             string jahr = Convert.ToString(dasJahr);
             string Dateiname;
             Dateiname = jahr.Insert(4, ".txt");
@@ -39,34 +37,27 @@ namespace KalenderWelt
 
             gibZeileAus("Kalender fuer das Jahr " + dasJahr);
             gibZeileAus();
+            string wochentage = String.Join(" ", HilfsKonstrukte.wochenTagNamenKurz);
 
+            if (HilfsKonstrukte.istSchaltJahr(dasJahr))
+            {
+                HilfsKonstrukte.tageImMonat[1] = 29; //Februar Feld im Array bei Schaltjahren auf 29 setzen
+            }
+            wochentagDesErstenImMonat = HilfsKonstrukte.startWochenTag(dasJahr);
             for (monat = 0; monat < 12; monat++) //Schleife für die Erstellung der Monate: 0=Jan bis 11=Dez
             {
                 gibZeileAus("      " + HilfsKonstrukte.monatsNamen[monat]);
-                gibZeileAus("MO DI MI DO FR SA SO");
+                gibZeileAus(wochentage);
 
-                if (dasJahr >= 1900) jahre = dasJahr - 1900; //vorarbeiten zur Berechnug des passenden Wochentags
-                else jahre = 1900 - dasJahr;
-                versatz = jahre + jahre / 4;
-                versatz = versatz + monate[monat];
-
-                if (HilfsKonstrukte.istSchaltJahr(dasJahr))
+                if (monat > 0 ) 
                 {
-                    versatz--;
-                    HilfsKonstrukte.tageImMonat[1] = 29; //Februar Feld im Array bei Schaltjahren auf 29 setzen
-                    monate[2] = 4; //versatz von März bei Schaltjahren korigieren, muß eins mehr sein als sonst
+                    wochentagDesErstenImMonat += HilfsKonstrukte.tageImMonat[monat - 1] % 7;
+                    wochentagDesErstenImMonat %= 7;
                 }
 
-                wochentag = versatz % 7; //Bestimmung des Wochentags durch die erechnete Veränderung seit 1900
-
-                //Die Woche beginnt immer Montags, nur der 1. eines Monats kann auf verschiedene Wochentage fallen. 
-                int ausgabePosition = wochentag;
-
-                //In Schaltjahen, ab Monat März "Ausgabe Bug" beheben 
-                if (HilfsKonstrukte.istSchaltJahr(dasJahr) && monat > 2)
-                {
-                    ausgabePosition = (ausgabePosition + 1) % 7;
-                }
+                //Die Woche beginnt immer Montags, nur der 1. eines Monats 
+                //kann auf verschiedene Wochentage fallen. 
+                int ausgabePosition = wochentagDesErstenImMonat;
 
                 string zeile = "".PadLeft(ausgabePosition * 3);
                 for (tag = 1; tag <= HilfsKonstrukte.tageImMonat[monat]; tag++) //solange Zähler kleiner gleich Wert aus Array Tage 
--- hilfskonstrukte.cs
+++ hilfskonstrukte.cs
@@ -5,6 +5,7 @@ namespace KalenderWelt
 {
 
     class HilfsKonstrukte {
+        public const int TAGE_IM_JAHR = 365;
         public static string[] monatsNamen = 
             {"Januar", "Februar", "Maerz", "April", 
              "Mai", "Juni", "Juli", "August", 
@@ -21,10 +22,20 @@ namespace KalenderWelt
               30, 31, 30, 31 };
 
         public static bool istSchaltJahr(int dasJahr) {
-            return (((dasJahr % 4 == 0) && 
-                    (!(dasJahr % 100 == 0))) || 
-                    ((dasJahr % 100 == 0) && 
+            return ((dasJahr % 4 == 0) && 
+                    ((dasJahr % 100 != 0) || 
                      (dasJahr % 400 == 0)));
+        }
+
+        public static int startWochenTag(int dasJahr) {
+            //15.10.1582 (Korrekturtag) war Fr
+            //-> 1.1.1583 Sa=5
+            int wochenTag = 5;
+            for (int i = 1583; i < dasJahr; ++i) {
+                wochenTag += TAGE_IM_JAHR + (istSchaltJahr(i) ? 1 : 0);
+                wochenTag %= 7;
+            }
+            return wochenTag;
         }
     } //ende class HilfsKonstrukte
 } //ende namespace KalenderWelt
