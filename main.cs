8f2e6f70cb22d4ffd58f35a0a4f3723981f3d409
christoph1717 <c.einsfeld@gmx.de>
2012-01-11 15:54:13 +0100

    Test Log und Eingabeüberprüfung

--- main.cs
+++ main.cs
@@ -9,19 +9,35 @@ namespace KalenderWelt
 
         public static int Main()
         {
-            Console.WriteLine("Das Programm berechnet Kalender für die Jahre 1583 bis 4000");
-            Console.Write("Jahr eingeben ");
-            int eingabejahr = Convert.ToInt32(Console.ReadLine());
+            string eingabe = "";
+            int eingabejahr = 0;
 
-            if ((eingabejahr < 1583) || (eingabejahr > 4000)) Console.WriteLine("Falsche Eingabe. Jahr nicht im gueltigen Bereich.");
-            else
+            Console.WriteLine("Das Programm berechnet Kalender für die Jahre 1583 bis 4000");
+            Console.Write("Jahr eingeben:  ");
+            eingabe = Console.ReadLine(); //erstmal als string speichern, damit auch eigegebene Buchstaben kein Problem sind
+
+            while (!int.TryParse(eingabe, out eingabejahr)) //Solange die Eingabe nicht korrekt ist wird weiter abgefragt
             {
-                DateiUndKonsolenAusgabe ausgabe = new DateiUndKonsolenAusgabe();
-                ausgabe.gibAus(eingabejahr);
-            } //ende if Eingabe gültig
+                Console.WriteLine("Falsche Eingabe, da Buchstaben nicht möglich sind.");
+                Console.Write("Bitte geben sie das Jahr erneut ein:  ");
+                eingabe = Console.ReadLine();
+            } //ende Eingabe Buchstaben Prüfung
+
+            while ((eingabejahr < 1583) || (eingabejahr > 4000)) //Wiederhole solange bis Eingabe im gültigen Bereich ist  
+            {
+                Console.WriteLine("Falsche Eingabe. Jahr ist nicht im gueltigen Bereich.");
+                Console.Write("Bitte geben sie das Jahr erneut ein:  ");
+                eingabe = Console.ReadLine();
+                int.TryParse(eingabe, out eingabejahr);
+            } //ende Eingabe Bereich Prüfung
+
+            DateiUndKonsolenAusgabe ausgabe = new DateiUndKonsolenAusgabe();
+            Console.WriteLine();
+            Console.WriteLine();
+            ausgabe.gibAus(eingabejahr);
             Console.ReadLine();
             return 0;
+
         } //ende main()
     } //ende class KalenderErzeuger
 } //ende namespace KalenderWelt
-
