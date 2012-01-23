a72d273155be428915975b6bd498450e16748dc6
rumpelart <rumpelart@gmx.de>
2012-01-23 02:10:09 +0100

    Beispiel für Einlesen von jaehrlichen Ereignissen aus xml

--- main.cs
+++ main.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.XPath;
 
 namespace KalenderWelt
 {
@@ -64,7 +66,30 @@ namespace KalenderWelt
             //Eintrag Experimente
             List<Eintrag> eintraege = new List<Eintrag> ();
             eintraege.Add(new EinmaligerTermin("beruflicher Termin", new DateTime(2012,1,23)));
-            eintraege.Add(new JaehrlichesEreignisAnFestemTag("Christophs Geburtstag", 6, 30));
+
+            //aus xml laden
+            XmlDocument doc = new XmlDocument();
+            doc.Load("input/geburtstage.xml");
+            XmlElement wurzel = doc.DocumentElement;
+            XmlNodeList eintragListe = wurzel.SelectNodes("./JaehrlichesEreignisAnFestemTag");
+            Console.WriteLine("anzahl der Eintraege in der xml " + eintragListe.Count); 
+            foreach (XmlNode eintrag in eintragListe)
+            {
+                //Console.WriteLine(eintrag.Attributes["titel"].Value);
+                //Console.WriteLine(eintrag.SelectSingleNode("./monat").OuterXml);
+                //Console.WriteLine(eintrag.SelectSingleNode("./monat").FirstChild.Value);
+                string titel = eintrag.Attributes["titel"].Value;
+                int monat, tag;
+                if (!int.TryParse(eintrag.SelectSingleNode("./monat").FirstChild.Value, out monat)) {
+                    Console.WriteLine("Monat ist keine Zahl");
+                }
+                if (!int.TryParse(eintrag.SelectSingleNode("./tag").FirstChild.Value, out tag)) {
+                    Console.WriteLine("Tag ist keine Zahl");
+                }
+                eintraege.Add(new JaehrlichesEreignisAnFestemTag(titel, monat, tag)); 
+            }
+
+            Console.WriteLine("alle Eintraege: ");
             foreach (Eintrag meinEintrag in eintraege) {
                 Console.WriteLine("Eintrag: " + meinEintrag.toString());
             }
